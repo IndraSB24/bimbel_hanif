c886134efbb8199cf51eed30acb1ad6041ef9025
Muhahanif23 <tatadot23@gmail.com>
2025-11-29 15:38:16 +0700

    Update latihan2.php

--- web/PHP/pengenalan_sintaks_dasar/latihan2.php
+++ web/PHP/pengenalan_sintaks_dasar/latihan2.php
@@ -10,11 +10,14 @@ $nominaldiskon = (1200000*25)/100;
 $hargaakhir = ($harga_awal-$nominaldiskon);
 $biayaiklan = ($biaya_iklan_harian*$durasi_promosi_hari);
 $profitperpeserta = ($hargaakhir-$biayaiklan/$jumlah_peserta);
+$h_akhir_fmt = number_format($hargaakhir, 0, ',', '.');
+$biayaiklanfmt = number_format($biayaiklan, 2, ',', '.');
+$profitpesertafmt = number_format($profitperpeserta, 2, ',', '.');
 echo "Kami meluncurkan promosi $nama_produk pada tanggal $tanggal_sekarang.
 Dengan harga normal $harga_awal, kami memberikan diskon besar sebesar Rp$persen_diskon%, 
 yang berarti setiap pelanggan menghemat $nominaldiskon secara nominal.
-Setelah diskon, harga jual bersihnya menjadi $hargaakhir. Hingga saat ini, kami telah berhasil menarik $jumlah_peserta peserta.
-Total pengeluaran untuk iklan selama $durasi_promosi_hari hari adalah $biayaiklan.
+Setelah diskon, harga jual bersihnya menjadi $h_akhir_fmt. Hingga saat ini, kami telah berhasil menarik $jumlah_peserta peserta.
+Total pengeluaran untuk iklan selama $durasi_promosi_hari hari adalah $biayaiklanfmt.
 Dengan asumsi biaya iklan terbagi rata, profit kotor rata-rata yang kami dapatkan per peserta adalah
-$profitperpeserta per unit, ini adalah angka yang menjanjikan! "
+$profitpesertafmt per unit, ini adalah angka yang menjanjikan! "
 ?>
