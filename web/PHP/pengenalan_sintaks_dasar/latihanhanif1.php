126c60504536d26ea468576593b292fb1a239b7d
Muhahanif23 <tatadot23@gmail.com>
2025-11-29 14:59:31 +0700

    Update latihanhanif1.php

--- web/PHP/pengenalan_sintaks_dasar/latihanhanif1.php
+++ web/PHP/pengenalan_sintaks_dasar/latihanhanif1.php
@@ -6,6 +6,6 @@ $sepuluh = 10;
 $lima = 5;
 $hasil = ($sepuluh/$lima);
 echo "$angkaplus $sepuluh dan $lima adalah:" . ($sepuluh+$lima) . "<br>";
-echo "$angkaminus $sepuluh dan $lima adalah" . ($sepuluh-$lima) . "<br>";
-echo "$angkabagi $sepuluh dan $lima adalah $hasil";
+echo "$angkaminus $sepuluh dan $lima adalah:" . ($sepuluh-$lima) . "<br>";
+echo "$angkabagi $sepuluh dan $lima adalah: $hasil";
 ?>
