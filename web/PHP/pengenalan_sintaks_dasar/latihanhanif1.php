2791bd6cc5f87f9b9901adfa80295470e98cf171
Muhahanif23 <tatadot23@gmail.com>
2025-11-29 14:58:54 +0700

    cmt

---
+++ web/PHP/pengenalan_sintaks_dasar/latihanhanif1.php
@@ -0,0 +1,11 @@
+<?php 
+$angkaplus = "Penjumlahan dari";
+$angkaminus = "Pengurangan dari";
+$angkabagi = "pembagian dari";
+$sepuluh = 10;
+$lima = 5;
+$hasil = ($sepuluh/$lima);
+echo "$angkaplus $sepuluh dan $lima adalah:" . ($sepuluh+$lima) . "<br>";
+echo "$angkaminus $sepuluh dan $lima adalah" . ($sepuluh-$lima) . "<br>";
+echo "$angkabagi $sepuluh dan $lima adalah $hasil";
+?>
--- web/form/hanif3.php
+++ web/form/hanif3.php
@@ -47,6 +47,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </body>
 </html>";
 }
+//hfghjfg
+
+#catatan
+
+/*
+*/
 ?>
 
 
